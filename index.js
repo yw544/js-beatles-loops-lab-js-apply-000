2fba7bc65ef2a36166c1a48d0b173372937364c4
York Wang <yw544@nyu.edu>
2017-11-29 09:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function theBeatlesPlay(musicians, instruments)
 {
   var play = [];
 
-  for (let i = 0; i < musicians.length; i++)
-  {
+  for (let i = 0; i < musicians.length; i++) {
     play.push(musicians[i], + " plays ", + instruments[i])
   }
 return play;
